44a9ee72cae987ef1ff65c77c1ee37701d88ab08
Regulus <alassaneseck94@gmail.com>
2022-07-29 16:13:13 +0000

    Iniialisation oof the project

---
+++ src/main/java/com/regulus/schoolmngmt/SchoolMngmtApplication.java
@@ -0,0 +1,13 @@
+package com.regulus.schoolmngmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMngmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMngmtApplication.class, args);
+	}
+
+}
